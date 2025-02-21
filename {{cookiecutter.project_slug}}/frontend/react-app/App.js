2a30545066cf32c3001f2213863b056ae44333f1
shiying <shiying.wei@ucl.ac.uk>
2025-02-21 12:14:23 +0000

    Apply template code for routing backend to frontend

--- {{cookiecutter.project_slug}}/frontend/react-app/App.js
+++ {{cookiecutter.project_slug}}/frontend/react-app/App.js
@@ -1,60 +1,18 @@
-import { Button, IconButton } from "@mui/material";
-import "./App.css";
-import { Dehaze as DehazeIcon } from "@mui/icons-material";
-import { useState } from "react";
-import { LoginModal } from "./src/LoginSignup/LoginModal";
-import { SideMenu } from "./src/Menus/SideMenu";
-import { Footer } from "./src/Menus/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
-	const [openModal, setOpenModal] = useState(false);
-	const [openMenu, setOpenMenu] = useState(false);
+    const [message, setMessage] = useState("");
 
-	const handleOpenModal = () => {
-		setOpenModal(true);
-	};
+    useEffect(() => {
+        fetch("/api/hello/")
+            .then((response) => response.json())
+            .then((data) => setMessage(data.message))
+            .catch((error) => console.error(error));
+    }, []);
 
-	const handleCloseModal = () => {
-		setOpenModal(false);
-	};
-
-	const handleOpenMenu = () => {
-		setOpenMenu(true);
-	};
-
-	const handleCloseMenu = () => {
-		setOpenMenu(false);
-	};
-
-	return (
-		<div className="App">
-			<header className="App-header">
-				<IconButton
-					className="btn btn--menu"
-					variant="outlined"
-					onClick={handleOpenMenu}
-				>
-					<DehazeIcon color="primary" />
-				</IconButton>
-				{/* optional side menu */}
-				<SideMenu openMenu={openMenu} handleCloseMenu={handleCloseMenu} />
-				<p>Optional titlebar text here</p>
-				<div>
-					<Button
-						className="btn btn--login"
-						variant="text"
-						onClick={handleOpenModal}
-					>
-						Login
-					</Button>
-					<LoginModal open={openModal} handleClose={handleCloseModal} />
-				</div>
-			</header>
-
-			<main></main>
-			<Footer />
-		</div>
-	);
+    return <h1>{message}</h1>;
 }
 
 export default App;
+
+
