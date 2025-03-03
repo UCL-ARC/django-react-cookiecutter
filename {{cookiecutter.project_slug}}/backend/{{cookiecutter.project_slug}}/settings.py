7cee39cf124f175416fd2c05b425b688eb423e2b
shiying <shiying.wei@ucl.ac.uk>
2025-03-03 16:33:41 +0000

    Clean up debugging codes

--- {{cookiecutter.project_slug}}/backend/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/backend/{{cookiecutter.project_slug}}/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
